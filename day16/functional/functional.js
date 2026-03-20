b00965d359abaeda1fdc54c16fd7fb687750213f
jinpro <jinpro0318@gmail.com>
2026-03-20 15:19:51 +0900

    260320 update

--- day16/functional/functional.js
+++ day16/functional/functional.js
@@ -6,7 +6,14 @@ const result2 = add("나는", "누구다");
 const result3 = add(true, false);
 console.log(result1, result2, result3);
 
+// const addF = (a) => {
+//   const addF2 = (b) => {
+//     const result = a + b;
+//     return result;
+//   };
+//   return addF2;
+// };
 const addF = (a) => (b) => a + b;
-const add5 = addF(5);
-const add5and10 = add5(10);
+const add5 = addF(5); // (b) => 5 + b;
+const add5and10 = add5(10); // 5 + 10;
 console.log(add5and10);
