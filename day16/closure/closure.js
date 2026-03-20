a81ed4dfa1cb1eda330f5646a67fb107e1cd90f3
jinpro <jinpro0318@gmail.com>
2026-03-20 12:55:52 +0900

    260320 update

---
+++ day16/closure/closure.js
@@ -0,0 +1,23 @@
+// 전역변수와 지역변수
+let globalVar = "I am global";
+
+function myFunction() {
+  let localVar = "I am local";
+  console.log(globalVar); // 출력
+  console.log(localVar); // 출력
+}
+myFunction(); // 함수의 생애주기 lifecycle
+console.log(globalVar); // 출력
+// console.log(localVar); // 오류
+
+// 클로저
+const x = 1;
+function outer() {
+  const x = 10;
+  const inner = function () {
+    console.log(x);
+  };
+  return inner;
+}
+const innerFunction = outer();
+innerFunction(); // 중첩함수
